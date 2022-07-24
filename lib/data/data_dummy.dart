0e5bc8525621e8c19dd5262d18984ab52186fef1
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-24 11:07:16 -0500

    Header 5.8

--- lib/data/data_dummy.dart
+++ lib/data/data_dummy.dart
@@ -20,28 +20,28 @@ List<Map<String, dynamic>> categories = [
 
 List<Map<String, dynamic>> places = [
   {
-    "image": "",
-    "name": "",
+    "image": "https://www.travelandleisure.com/thmb/ipVEI18VvgYxIlUl5yS0GgI7Pus=/650x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/belize-GOWHERE221121-776f36756ab34558a30ad6c05699e70a.jpg",
+    "name": "Belize",
     "rate": 5.0,
     "views": 24343,
   },
   {
-    "image": "",
-    "name": "",
-    "rate": 5.0,
-    "views": 24343,
+    "image": "https://www.travelandleisure.com/thmb/UHbXtK8fzMOT753ZUMx8VMdB5SU=/650x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/moskito-island-GOWHERE221121-5229a939acd245b69264f341e851157f.jpg",
+    "name": "British Virgin Islands",
+    "rate": 4.1,
+    "views": 12453,
   },
   {
-    "image": "",
-    "name": "",
-    "rate": 5.0,
-    "views": 24343,
+    "image": "https://www.travelandleisure.com/thmb/RCHPd29FJykUla33-gS90q94KsY=/650x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/edinburgh-scotland-GOWHERE221121-c9d5a0c4a53c4870861675cd0d512235.jpg",
+    "name": "Edinburgh, Scotland",
+    "rate": 3.0,
+    "views": 1124,
   },
   {
-    "image": "",
-    "name": "",
-    "rate": 5.0,
-    "views": 24343,
+    "image": "https://www.travelandleisure.com/thmb/phZvUN6ripUFw5qkx0ElfXKc4-A=/650x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/taughannock-finger-lakes-new-york-GOWHERE221121-463fc61949e44c7882b02aa6fbb5188a.jpg",
+    "name": "Finger Lakes, New York",
+    "rate": 4.6,
+    "views": 54323,
   },
 
 ];
