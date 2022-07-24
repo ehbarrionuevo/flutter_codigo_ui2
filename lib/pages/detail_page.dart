8a43e7a4743aa200f4014eeb4aa5857c7816c011
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-24 11:51:05 -0500

    Header 6.3

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -6,6 +6,37 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 400.0,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.only(bottomRight: Radius.circular(46.0,),),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage("https://www.travelandleisure.com/thmb/ipVEI18VvgYxIlUl5yS0GgI7Pus=/650x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/belize-GOWHERE221121-776f36756ab34558a30ad6c05699e70a.jpg"),
+              ),
+            ),
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.red,
+                        Colors.blue,
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
