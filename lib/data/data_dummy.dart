9ee90a1fbeaa9af793807f81b88561199d6b6c8d
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-24 10:22:47 -0500

    Header 5.6

--- lib/data/data_dummy.dart
+++ lib/data/data_dummy.dart
@@ -5,17 +5,17 @@ List places = [];
 List<Map<String, dynamic>> categories = [
   {
     "text": "Art & Culture",
-    "color": Colors.red,
-    "icon": Icons.add,
+    "color": Color(0xff5D6DFF),
+    "icon": Icons.ac_unit,
   },
   {
     "text": "Adventure",
-    "color": Colors.red,
-    "icon": Icons.add,
+    "color": Color(0xff37D2FF),
+    "icon": Icons.location_on,
   },
   {
     "text": "Festival",
-    "color": Colors.red,
-    "icon": Icons.add,
+    "color": Color(0xff2687EE),
+    "icon": Icons.personal_injury_rounded,
   },
 ];
